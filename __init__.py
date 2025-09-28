ebb876e36fee67b10067432a2bc98fc8e16e50f7
RonaldoPinheiro2020 <ronaldo.pinheiro2009@gmail.com>
2025-09-28 15:05:02 -0300

    feat: enviar a arquivos __init__ (v0.1.0)

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# Esta vari\u00e1vel ser\u00e1 automaticamente atualizada pelo semantic-release
+__version__ = "0.1.2" 
+
+# Aqui voc\u00ea pode importar outras partes do seu pacote
+# from .core import ...
