203923cc7b6f9849f2f6459b01768431eccb2e51
RonaldoPinheiro2020 <ronaldo.pinheiro2009@gmail.com>
2025-09-28 15:52:59 -0300

    fix: corrigir erro no pyproject.toml

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # Esta vari\u00e1vel ser\u00e1 automaticamente atualizada pelo semantic-release
-__version__ = "0.1.2" 
+__version__ = "0.0.0" 
 
 # Aqui voc\u00ea pode importar outras partes do seu pacote
 # from .core import ...
